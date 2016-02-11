a466036a05c9951348f30a4c984fe57184b9858d
Shoeb Ahmed Adeel <shaan114@gmail.com>
2016-02-12 05:28:46 +0530

    Initialised the repo by adding four new files

---
+++ fps.py
@@ -0,0 +1,24 @@
+import cv2.cv as cv
+import time
+
+f= open("time_data.txt","w+")
+cv.NamedWindow("camera", 1)
+capture = cv.CaptureFromCAM(0)
+i = 0
+
+t0 = time.time()
+while True:
+	img = cv.QueryFrame(capture)
+	cv.ShowImage("camera", img)
+	cv.SaveImage('pic{:>05}.jpg'.format(i), img)
+	#cv.SaveImage(str(interval) + ".jpg", img)
+	ti = time.time()
+	if i==0:
+		f.write(str(0) + " s \n " )
+	else:
+		f.write(str(ti - t0) + " s \n ")
+	if cv.WaitKey(10) == 27:
+		break
+	i += 1
+
+f.close()
